9906075342468ebf749f4e0ece4bde3f22783f5c
Livia Coutinho <livia.coutinho@sou.inteli.edu.br>
2023-05-24 13:47:41 -0300

    Variables&Printing

---
+++ Section1 - Intro to Python/Variables&Printing.py
@@ -0,0 +1,37 @@
+"""Olá, this is a docstring"""
+# Instalei o autopep8 para formatação de código
+# ctrl + shift + p
+
+# Pylint extension
+# run linting
+
+# https://www.youtube.com/watch?v=7Kpd6eprz4k
+
+# print('Olá, pessoal')
+# print('whate l')
+
+##############################
+# 4. DEFINING VARIABLES AND PRINTING
+
+AGE = 30
+print (AGE)
+
+
+#Aprendizados obtidos com as meras duas linhas acima (todos através doss erros, não exatamente da aula)
+    #Para fins de documentação deve-se começar o código com um docstring (""""X"""").
+        #Caso começe com # (comentário simples, o pylint manda erro)
+    #  O pylint traz uma convenção de nomenclatura em letras maiúsculas.
+        #o estilo UPPER_CASE é geralmente usado para nomear constantes
+    #A última linha do código deve estar vazia
+
+#Variable names can contain underscores and numbers
+#But they cannot start with a number
+#This type of separation SNAKE_CASE
+
+#PI = 3.14159
+#RADIANS_TO_DEGREES = 180/PI
+
+#We can change what values names refers to
+
+##############################
+
